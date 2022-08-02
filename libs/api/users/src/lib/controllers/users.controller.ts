0cd9bc5e559a4c47b3e3c6e4c46112a410bb0e79
Jeff Kwok <jkwok97@gmail.com>
2022-08-02 08:25:12 -0600

    test

--- libs/api/users/src/lib/controllers/users.controller.ts
+++ libs/api/users/src/lib/controllers/users.controller.ts
@@ -34,7 +34,7 @@ export class UsersController {
   }
 
   @Get('/userId/:id')
-  async getUserById(@Param('id') param) {
+  async getUserById(@Param() param) {
     console.log(parseInt(param.id));
     const user = await this.usersService.findUserById(parseInt(param.id));
     if (!user) {
