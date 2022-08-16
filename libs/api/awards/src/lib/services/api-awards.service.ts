36f2c0f7df5ac3594deddda7fdcade283fbb8861
Jeff Kwok <jkwok97@gmail.com>
2022-08-16 10:28:21 -0600

    award api test

--- libs/api/awards/src/lib/services/api-awards.service.ts
+++ libs/api/awards/src/lib/services/api-awards.service.ts
@@ -1,4 +1,4 @@
-import { AwardDto, Awards_V2, AwardTypeEnum } from '@cha/shared/entities';
+import { Awards_V2, AwardTypeEnum } from '@cha/shared/entities';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -11,34 +11,50 @@ export class ApiAwardsService {
 
   async getChampions(): Promise<Awards_V2[]> {
     return await this.repo.find({
-      relations: ['users_id', 'team_id', 'player_id'],
+      relations: ['users_id', 'team_id'],
       where: { award_type: AwardTypeEnum.CHAMPION },
     });
-
-    // return await this.repo.findBy({ award_type: AwardTypeEnum.CHAMPION });
   }
 
   async getScorerAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.SCORER });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id', 'player_id'],
+      where: { award_type: AwardTypeEnum.SCORER },
+    });
   }
 
   async getDefenseAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.DEFENSE });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id', 'player_id'],
+      where: { award_type: AwardTypeEnum.DEFENSE },
+    });
   }
 
   async getRookieAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.ROOKIE });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id', 'player_id'],
+      where: { award_type: AwardTypeEnum.ROOKIE },
+    });
   }
 
   async getGoalieAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.GOALIE });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id', 'player_id'],
+      where: { award_type: AwardTypeEnum.GOALIE },
+    });
   }
 
   async getGmAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.GM });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id'],
+      where: { award_type: AwardTypeEnum.GM },
+    });
   }
 
   async getSeasonAwards(): Promise<Awards_V2[]> {
-    return await this.repo.findBy({ award_type: AwardTypeEnum.SEASON });
+    return await this.repo.find({
+      relations: ['users_id', 'team_id'],
+      where: { award_type: AwardTypeEnum.SEASON },
+    });
   }
 }
