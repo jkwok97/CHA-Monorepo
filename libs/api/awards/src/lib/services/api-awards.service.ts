ac22cc992dc036d37af6a2dedad4cda71aa2b7fa
Jeff Kwok <jkwok97@gmail.com>
2022-08-17 12:38:01 -0600

    test

--- libs/api/awards/src/lib/services/api-awards.service.ts
+++ libs/api/awards/src/lib/services/api-awards.service.ts
@@ -10,7 +10,9 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ApiAwardsService {
   constructor(
-    @InjectRepository(Awards_V2) private repo: Repository<Awards_V2>
+    @InjectRepository(Awards_V2) private repo: Repository<Awards_V2>,
+    @InjectRepository(Players_Stats_V2)
+    private statsRepo: Repository<Players_Stats_V2>
   ) {}
 
   async getChampions(): Promise<Awards_V2[]> {
@@ -25,34 +27,60 @@ export class ApiAwardsService {
   }
 
   async getScorerAwards() {
-    // const scorers = await this.repo.find({
-    //   relations: ['users_id', 'team_id', 'player_id', 'award_type'],
-    //   where: {
-    //     award_type: {
-    //       id: AwardTypeEnum.SCORER,
-    //     },
-    //   },
-    // });
+    const scorers = await this.repo.find({
+      relations: ['users_id', 'team_id', 'player_id', 'award_type'],
+      where: {
+        award_type: {
+          id: AwardTypeEnum.SCORER,
+        },
+      },
+    });
 
-    return await this.repo
-      .createQueryBuilder('awards_v2')
-      .leftJoinAndSelect('awards_v2.award_type', 'award_type')
-      .where('award_type.id = :award_type', {
-        award_type: AwardTypeEnum.SCORER,
-      })
-      .leftJoinAndSelect('awards_v2.team_id', 'teams')
-      .leftJoinAndSelect('awards_v2.users_id', 'users')
-      .leftJoinAndSelect('awards_v2.player_id', 'players')
-      .leftJoin(
-        Players_Stats_V2,
-        'playerStats',
-        'awards_v2.player_id = playerStats.player_id and awards_v2.cha_season = playerStats.playing_year'
+    console.log(scorers);
+
+    const scorerStats = await this.statsRepo
+      .createQueryBuilder('playerStats')
+      .where(
+        'scorers.getQuery().player_id.id = playerStats.player_id and scorers.getQuery().cha_season = playerStats.playing_year'
       )
-      .where('playerStats.season_type = :season_type', {
-        season_type: 'Regular',
-      })
-      .orderBy('awards_v2.display_season', 'DESC')
       .getMany();
+
+    console.log(scorerStats);
+
+    return {
+      result: {
+        scorers: {
+          ...scorers,
+          stats: scorerStats,
+        },
+      },
+    };
+
+    // return await this.repo
+    //   .createQueryBuilder('awards_v2')
+    //   .leftJoinAndSelect('awards_v2.award_type', 'award_type')
+    //   .where('award_type.id = :award_type', {
+    //     award_type: AwardTypeEnum.SCORER,
+    //   })
+    //   .leftJoinAndSelect('awards_v2.team_id', 'teams')
+    //   .leftJoinAndSelect('awards_v2.users_id', 'users')
+    //   .leftJoinAndSelect('awards_v2.player_id', 'players')
+    //   .leftJoin((sub) => {
+    //     const subQuery = sub
+    //       .subQuery()
+    //       .select('playerStats.*', 'playerStats')
+    //       .from(Players_Stats_V2, 'playerStats')
+    //       .where(
+    //         'awards_v2.player_id = playerStats.player_id and awards_v2.cha_season = playerStats.playing_year'
+    //       )
+    //       .getOne();
+    //       return subQuery;
+    //   })
+    //   .where('playerStats.season_type = :season_type', {
+    //     season_type: 'Regular',
+    //   })
+    //   .orderBy('awards_v2.display_season', 'DESC')
+    //   .getMany();
   }
 
   async getDefenseAwards(): Promise<Awards_V2[]> {
