6810681476282af71e52c8f3f15b60aa64062c0d
Jeff Kwok <jkwok97@gmail.com>
2022-08-17 13:04:41 -0600

    test

--- libs/api/awards/src/lib/services/api-awards.service.ts
+++ libs/api/awards/src/lib/services/api-awards.service.ts
@@ -17,7 +17,7 @@ export class ApiAwardsService {
 
   async getChampions(): Promise<Awards_V2[]> {
     return await this.repo.find({
-      relations: ['users_id', 'team_id', 'award_type'],
+      relations: ['users_id', 'team_id', 'award_type', 'cha_season'],
       where: {
         award_type: {
           id: AwardTypeEnum.CHAMPION,
@@ -28,13 +28,7 @@ export class ApiAwardsService {
 
   async getScorerAwards() {
     const scorers = await this.repo.find({
-      relations: [
-        'users_id',
-        'team_id',
-        'player_id',
-        'award_type',
-        'cha_season',
-      ],
+      relations: ['users_id', 'team_id', 'player_id', 'award_type'],
       where: {
         award_type: {
           id: AwardTypeEnum.SCORER,
