73c7bc9c8ce029e9d8ac30c769cd25fd603b87ab
Jeff Kwok <jkwok97@gmail.com>
2022-08-17 10:02:06 -0600

    fix

--- libs/api/awards/src/lib/api-awards.module.ts
+++ libs/api/awards/src/lib/api-awards.module.ts
@@ -1,4 +1,4 @@
-import { Awards_V2 } from '@cha/shared/entities';
+import { Awards_V2, Players_Stats_V2 } from '@cha/shared/entities';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AwardsController } from './controllers';
@@ -6,7 +6,7 @@ import { AwardsMiddleware } from './middlewares/awards.middleware';
 import { ApiAwardsService } from './services/api-awards.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Awards_V2])],
+  imports: [TypeOrmModule.forFeature([Awards_V2, Players_Stats_V2])],
   controllers: [AwardsController],
   providers: [ApiAwardsService],
 })
