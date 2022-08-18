8b5d64c207aca5e1a18267bcef5045cc398e8a7e
Jeff Kwok <jkwok97@gmail.com>
2022-08-18 15:55:13 -0600

    create blade divider

---
+++ libs/blade/angular/ui/divider/README.md
@@ -0,0 +1,7 @@
+# blade-angular-ui-divider
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test blade-angular-ui-divider` to execute the unit tests.
---
+++ libs/blade/angular/ui/divider/src/lib/divider/divider.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DividerModule } from 'primeng/divider';
+
+@Component({
+  selector: 'blade-divider',
+  standalone: true,
+  imports: [CommonModule, DividerModule],
+  templateUrl: './divider.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DividerComponent {
+  @Input() type = 'solid';
+  @Input() layout = 'horizontal';
+  @Input() align = 'center';
+}
