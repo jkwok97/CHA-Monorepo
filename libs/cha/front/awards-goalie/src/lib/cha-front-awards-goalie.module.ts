239df8d393daf2023440613bff0f7a6ef6b8f617
Jeff Kwok <jkwok97@gmail.com>
2022-08-18 11:01:38 -0600

    goalie awards page

--- libs/cha/front/awards-goalie/src/lib/cha-front-awards-goalie.module.ts
+++ libs/cha/front/awards-goalie/src/lib/cha-front-awards-goalie.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeAwardCardComponent } from '@blade/angular/ui/award-card';
+
 import { ChaFrontAwardsGoalieRoutingModule } from './cha-front-awards-goalie-routing.module';
 
 import { AwardsGoalieComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontAwardsGoalieRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontAwardsGoalieRoutingModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+    BladeAwardCardComponent,
+  ],
   declarations: [AwardsGoalieComponent],
 })
 export class ChaFrontAwardsGoalieModule {}
--- libs/cha/front/awards-goalie/src/lib/containers/awards-goalie/awards-goalie.component.ts
+++ libs/cha/front/awards-goalie/src/lib/containers/awards-goalie/awards-goalie.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AwardsFacade } from '@cha/domain/core';
+import { AwardDto } from '@cha/shared/entities';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cha-front-awards-goalie',
@@ -6,4 +9,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./awards-goalie.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AwardsGoalieComponent {}
+export class AwardsGoalieComponent implements OnInit {
+  goalies$: Observable<AwardDto[]>;
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+
+  constructor(private awardsFacade: AwardsFacade) {
+    this.goalies$ = this.awardsFacade.awards$;
+    this.isLoading$ = this.awardsFacade.isLoading$;
+    this.isLoaded$ = this.awardsFacade.isLoaded$;
+  }
+
+  ngOnInit(): void {
+    this.awardsFacade.getGoalies();
+  }
+}
