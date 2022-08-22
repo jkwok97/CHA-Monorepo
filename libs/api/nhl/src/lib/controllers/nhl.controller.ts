cf30ca170c64995c311c93146faa28dd79aaadb6
Jeff Kwok <jkwok97@gmail.com>
2022-08-22 13:28:54 -0600

    test

--- libs/api/nhl/src/lib/controllers/nhl.controller.ts
+++ libs/api/nhl/src/lib/controllers/nhl.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { ApiNhlService } from '../services';
 
 @Controller('nhl')
 export class NhlController {
   constructor(private nhlService: ApiNhlService) {}
 
-  @Get('/nhl-leaders')
-  async getNhlLeaders(@Query() query) {
+  @Get('/nhl-leaders/:season/:playerType/:statType')
+  async getNhlLeaders(@Param() param) {
     const nhlLeaders = await this.nhlService.getNhlLeaders(
-      query.playerType,
-      query.statType,
-      query.season
+      param.playerType,
+      param.statType,
+      param.season
     );
 
     if (!nhlLeaders || nhlLeaders.length < 1) {
