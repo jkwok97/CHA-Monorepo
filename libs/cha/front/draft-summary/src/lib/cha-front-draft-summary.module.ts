d39f73eb7ad6222e8d275a47edbee8ee4e55bbb0
Jeff Kwok <jkwok97@gmail.com>
2022-08-22 08:28:58 -0600

    Entry Draft Table FE

--- libs/cha/front/draft-summary/src/lib/cha-front-draft-summary.module.ts
+++ libs/cha/front/draft-summary/src/lib/cha-front-draft-summary.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+import { BladeAngularUiTableModule } from '@blade/angular/ui/table';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+
+import { ProgressBarModule } from 'primeng/progressbar';
+
 import { ChaFrontDraftSummaryRoutingModule } from './cha-front-draft-summary-routing.module';
 
 import { DraftSummaryComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontDraftSummaryRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontDraftSummaryRoutingModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+    BladeDividerComponent,
+    BladeAngularUiCardModule,
+    BladeAngularUiTableModule,
+  ],
   declarations: [DraftSummaryComponent],
 })
 export class ChaFrontDraftSummaryModule {}
--- libs/cha/front/draft-summary/src/lib/containers/draft-summary/draft-summary.component.ts
+++ libs/cha/front/draft-summary/src/lib/containers/draft-summary/draft-summary.component.ts
@@ -1,9 +1,73 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LeagueDataFacade } from '@cha/domain/core';
+import { DraftPickDto, TeamDto } from '@cha/shared/entities';
+import { Observable } from 'rxjs';
+import { DraftSummaryFacade } from '../../+state/draft-summary.facade';
 
 @Component({
   selector: 'cha-front-draft-summary',
   templateUrl: './draft-summary.component.html',
-  styleUrls: ['./draft-summary.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DraftSummaryComponent {}
+export class DraftSummaryComponent implements OnInit {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  draftPicks$: Observable<DraftPickDto[]>;
+  teams$: Observable<TeamDto[]>;
+
+  teams!: TeamDto[];
+
+  tableColumns = [
+    { field: 'draft_year', header: 'Year' },
+    { field: 'draft_round', header: 'Round' },
+    { field: 'draft_overall', header: 'Overall' },
+    { field: 'player', header: 'Player' },
+    { field: 'team', header: 'Team' },
+  ];
+
+  filteringOptions = [
+    'draft_year',
+    'draft_round',
+    'draft_overall',
+    'player',
+    'team',
+  ];
+
+  constructor(
+    private draftSummaryFacade: DraftSummaryFacade,
+    private leagueDataFacade: LeagueDataFacade
+  ) {
+    this.isLoaded$ = this.draftSummaryFacade.isLoaded$;
+    this.isLoading$ = this.draftSummaryFacade.isLoading$;
+    this.draftPicks$ = this.draftSummaryFacade.draftPicks$;
+    this.teams$ = this.leagueDataFacade.leagueTeams$;
+  }
+
+  ngOnInit(): void {
+    this.draftSummaryFacade.getDraftPicks();
+  }
+
+  mapItems(items: DraftPickDto[]) {
+    return items.map((item: DraftPickDto) => ({
+      ...item,
+      team: `${item.city} ${item.nickname}`,
+      player: `${item.firstname} ${item.lastname}`,
+      playerImg: this.getPlayerPicture(item.nhl_id),
+      teamImg: this.getString(item.teamlogo),
+    }));
+  }
+
+  // TODO TEMP WILL NEED TO ADJUST USER TEAM LOGO STRING WHEN READY
+  getString(urlString: string) {
+    const temp = urlString.split('/');
+    return `assets/${temp[temp.length - 1]}`;
+  }
+
+  getPlayerPicture(id: number) {
+    if (id) {
+      return `https://cms.nhl.bamgrid.com/images/headshots/current/168x168/${id}@2x.jpg`;
+    } else {
+      return;
+    }
+  }
+}
