9a3bc48fd05b8943a124ea9d95b5d7ffdfbd5b31
Jeff Kwok <jkwok97@gmail.com>
2022-08-29 15:14:50 -0600

    FE make Api Call for all team stats

--- libs/cha/front/stats-team-all/src/lib/cha-front-stats-team-all.module.ts
+++ libs/cha/front/stats-team-all/src/lib/cha-front-stats-team-all.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { ProgressBarModule } from 'primeng/progressbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+
+import { StatsTeamAllEffects } from './+state/stats-team-all.effects';
+import { reducer } from './+state/stats-team-all.reducer';
+import { StatsTeamAllFacade } from './+state/stats-team-all.facade';
+
 import { ChaFrontStatsTeamAllRoutingModule } from './cha-front-stats-team-all-routing.module';
 
-import { StatsTeamAllComponent } from './containers';
 import { StatsTeamAllService } from './services';
 
+import { StatsTeamAllComponent } from './containers';
+
 @NgModule({
-  imports: [CommonModule, ChaFrontStatsTeamAllRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontStatsTeamAllRoutingModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+
+    StoreModule.forFeature('stats-team-all', reducer),
+    EffectsModule.forFeature([StatsTeamAllEffects]),
+  ],
   declarations: [StatsTeamAllComponent],
-  providers: [StatsTeamAllService],
+  providers: [StatsTeamAllService, StatsTeamAllFacade, StatsTeamAllEffects],
 })
 export class ChaFrontStatsTeamAllModule {}
--- libs/cha/front/stats-team-all/src/lib/containers/stats-team-all/stats-team-all.component.ts
+++ libs/cha/front/stats-team-all/src/lib/containers/stats-team-all/stats-team-all.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { UserTeamFacade } from '@cha/domain/core';
+import { TeamDto } from '@cha/shared/entities';
+import { Observable, first } from 'rxjs';
+import { StatsTeamAllFacade } from '../../+state/stats-team-all.facade';
 
 @Component({
   selector: 'cha-front-stats-team-all',
@@ -6,4 +10,32 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./stats-team-all.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatsTeamAllComponent {}
+export class StatsTeamAllComponent implements OnInit {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  currentTeam$: Observable<TeamDto | undefined>;
+
+  backgroundColor!: string;
+
+  constructor(
+    private statsTeamAllFacade: StatsTeamAllFacade,
+    private userTeamFacade: UserTeamFacade
+  ) {
+    this.isLoaded$ = this.statsTeamAllFacade.isLoaded$;
+    this.isLoading$ = this.statsTeamAllFacade.isLoading$;
+
+    this.currentTeam$ = this.userTeamFacade.currentUserTeam$;
+
+    this.currentTeam$
+      .pipe(first())
+      .subscribe((userTeam: TeamDto | undefined) => {
+        if (userTeam) {
+          this.backgroundColor = userTeam.teamcolor;
+        }
+      });
+  }
+
+  ngOnInit(): void {
+    this.statsTeamAllFacade.getAllTeamStats();
+  }
+}
