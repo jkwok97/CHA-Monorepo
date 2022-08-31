e28729bdbc283af3888a89036b2d0e2705925c1a
Jeff Kwok <jkwok97@gmail.com>
2022-08-31 16:06:52 -0600

    fix teams summary routing

--- libs/cha/front/league-teams/src/lib/containers/league-teams/league-teams.component.ts
+++ libs/cha/front/league-teams/src/lib/containers/league-teams/league-teams.component.ts
@@ -2,8 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'cha-front-league-teams',
-  templateUrl: './league-teams.component.html',
-  styleUrls: ['./league-teams.component.scss'],
+  template: '<router-outlet></router-outlet>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeagueTeamsComponent {}
