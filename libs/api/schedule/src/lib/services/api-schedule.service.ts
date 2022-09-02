4414346e272d613ade773fbdc8dba0677c35dc3c
Jeff Kwok <jkwok97@gmail.com>
2022-09-02 13:15:53 -0600

    test last five record

--- libs/api/schedule/src/lib/services/api-schedule.service.ts
+++ libs/api/schedule/src/lib/services/api-schedule.service.ts
@@ -43,12 +43,63 @@ export class ApiScheduleService {
     return scheduleTeamInfo;
   }
 
+  private async getTeamLastFive(teamId: number, type: 'visitors' | 'home') {
+    let lastFive: Schedule_V2[];
+
+    if (type === 'visitors') {
+      lastFive = await this.repo.find({
+        where: {
+          vis_team_id: teamId,
+        },
+        order: {
+          game_day: 'DESC',
+        },
+        take: 5,
+      });
+    } else {
+      lastFive = await this.repo.find({
+        where: {
+          home_team_id: teamId,
+        },
+        order: {
+          game_day: 'DESC',
+        },
+        take: 5,
+      });
+    }
+
+    return await this.getRecord(lastFive, type);
+  }
+
+  private async getRecord(lastFive: Schedule_V2[], type: 'visitors' | 'home') {
+    const lastFiveRecord = [];
+
+    await lastFive.forEach((record: Schedule_V2) => {
+      if (type === 'visitors') {
+        if (record.vis_team_score > record.home_team_score) {
+          lastFiveRecord.push('W');
+        } else if (record.vis_team_score < record.home_team_score) {
+          lastFiveRecord.push('L');
+        } else {
+          lastFiveRecord.push('T');
+        }
+      }
+    });
+
+    return lastFiveRecord;
+  }
+
   private async setTeamInfo(array: Schedule_V2[]) {
     return await Promise.all(
       array.map(async (item) => ({
         ...item,
         visTeamInfo: await this.getTeamInfo(item.vis_team_id),
+        visTeamLastFive: await this.getTeamLastFive(
+          item.vis_team_id,
+          'visitors'
+        ),
         homeTeamInfo: await this.getTeamInfo(item.home_team_id),
+        homeTeamLastFive: await this.getTeamLastFive(item.vis_team_id, 'home'),
       }))
     );
   }
