bb4a99e1a3bed07f7cd2c4d60902fd85ba253d91
Jeff Kwok <jkwok97@gmail.com>
2022-09-02 15:47:23 -0600

    trigger

--- libs/cha/front/games-upcoming/src/lib/cha-front-games-upcoming.module.ts
+++ libs/cha/front/games-upcoming/src/lib/cha-front-games-upcoming.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ChaFrontGamesUpcomingRoutingModule } from './cha-front-games-upcoming-routing.module';
 
 import { GamesUpcomingComponent } from './containers';
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontGamesUpcomingRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontGamesUpcomingRoutingModule,
+    BladeAngularUiLayoutModule,
+    BladeDividerComponent,
+  ],
   declarations: [GamesUpcomingComponent],
 })
 export class ChaFrontGamesUpcomingModule {}
