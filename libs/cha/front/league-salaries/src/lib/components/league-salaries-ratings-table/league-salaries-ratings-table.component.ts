0bb2180ddaff4a81cf23eb0b267100aea70a589b
Jeff Kwok <jkwok97@gmail.com>
2022-09-12 20:42:14 -0600

    finish league salaries ratings page

--- libs/cha/front/league-salaries/src/lib/components/league-salaries-ratings-table/league-salaries-ratings-table.component.ts
+++ libs/cha/front/league-salaries/src/lib/components/league-salaries-ratings-table/league-salaries-ratings-table.component.ts
@@ -1,7 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { DisplayFacade } from '@cha/domain/core';
 import { SalariesAndRatingsDto } from '@cha/shared/entities';
 import { first } from 'rxjs';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'cha-front-league-salaries-ratings-table',
@@ -9,14 +16,17 @@ import { first } from 'rxjs';
   styleUrls: ['./league-salaries-ratings-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeagueSalariesRatingsTableComponent {
+export class LeagueSalariesRatingsTableComponent implements OnInit {
   @Input() salaries!: SalariesAndRatingsDto[];
   @Input() type!: string;
+
+  @ViewChild('dt') dt: Table | undefined;
 
   isMobile = false;
   first = 0;
   rows = 50;
   totalRecords = 0;
+  salariesForTable!: any;
 
   constructor(private displayFacade: DisplayFacade) {
     this.displayFacade.isMobile$
@@ -26,9 +36,26 @@ export class LeagueSalariesRatingsTableComponent {
       });
   }
 
+  ngOnInit(): void {
+    this.salariesForTable = this.mapItems(this.salaries);
+  }
+
+  mapItems(salaries: SalariesAndRatingsDto[]) {
+    return salaries.map((salary: SalariesAndRatingsDto) => ({
+      ...salary,
+      teamLogo: this.getString(salary.teamInfo.teamlogo),
+      teamName: `${salary.teamInfo.city} ${salary.teamInfo.nickname}`,
+      playerName: `${salary.player_id.firstname} ${salary.player_id.lastname}`,
+    }));
+  }
+
   // TODO TEMP WILL NEED TO ADJUST USER TEAM LOGO STRING WHEN READY
   getString(urlString: string) {
     const temp = urlString.split('/');
     return `assets/${temp[temp.length - 1]}`;
+  }
+
+  applyFilterGlobal(event: any, stringVal: string) {
+    this.dt?.filterGlobal((event.target as HTMLInputElement).value, stringVal);
   }
 }
