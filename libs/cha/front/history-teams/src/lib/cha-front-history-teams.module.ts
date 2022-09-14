725ce0e66cc0ee9bbf0580dd3a9f5939803ba796
Jeff Kwok <jkwok97@gmail.com>
2022-09-14 09:56:35 -0600

    start history teams page

--- libs/cha/front/history-teams/src/lib/cha-front-history-teams.module.ts
+++ libs/cha/front/history-teams/src/lib/cha-front-history-teams.module.ts
@@ -4,6 +4,13 @@ import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeButtonSelectComponent } from '@blade/angular/ui/button-select';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+
 import { HistoryTeamsEffects } from './+state/history-teams.effects';
 import { HistoryTeamsFacade } from './+state/history-teams.facade';
 import { reducer } from './+state/history-teams.reducer';
@@ -17,6 +24,11 @@ import { HistoryTeamsComponent } from './containers';
   imports: [
     CommonModule,
     ChaFrontHistoryTeamsRoutingModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+    BladeButtonSelectComponent,
+    BladeDividerComponent,
+    BladeAngularUiCardModule,
     StoreModule.forFeature('history-teams', reducer),
     EffectsModule.forFeature([HistoryTeamsEffects]),
   ],
--- libs/cha/front/history-teams/src/lib/containers/history-teams/history-teams.component.ts
+++ libs/cha/front/history-teams/src/lib/containers/history-teams/history-teams.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LeagueDataFacade } from '@cha/domain/core';
+import { StatTeamsHistoryDto } from '@cha/shared/entities';
+import { Observable, first } from 'rxjs';
+import { HistoryTeamsFacade } from '../../+state/history-teams.facade';
 
 @Component({
   selector: 'cha-front-history-teams',
@@ -6,4 +10,58 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./history-teams.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HistoryTeamsComponent {}
+export class HistoryTeamsComponent implements OnInit {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  allStats$: Observable<StatTeamsHistoryDto[]>;
+
+  seasonOption = 'Regular';
+
+  selectOptions = [
+    { label: 'Per Season', value: 'season' },
+    { label: 'All-time', value: 'all' },
+  ];
+
+  selectSeasonOptions = [
+    { label: 'Regular', value: 'Regular' },
+    { label: 'Playoffs', value: 'Playoffs', disabled: false },
+  ];
+
+  constructor(
+    private historyTeamsFacade: HistoryTeamsFacade,
+    private leagueDataFacade: LeagueDataFacade
+  ) {
+    this.isLoaded$ = this.historyTeamsFacade.isLoaded$;
+    this.isLoading$ = this.historyTeamsFacade.isLoading$;
+
+    this.allStats$ = this.historyTeamsFacade.allStats$;
+
+    this.leagueDataFacade.isOffSeason$
+      .pipe(first())
+      .subscribe((isOffSeason: boolean) => {
+        this.selectSeasonOptions = [
+          { label: 'Regular', value: 'Regular' },
+          { label: 'Playoffs', value: 'Playoffs', disabled: !isOffSeason },
+        ];
+      });
+  }
+
+  ngOnInit(): void {
+    this.historyTeamsFacade.getAllTimeTeamsStatBySeason('Regular');
+  }
+
+  onSeasonOptionChanged(option: string) {
+    this.seasonOption = option;
+    this.historyTeamsFacade.getAllTimeTeamsStatBySeason(option);
+  }
+
+  onOptionChanged(option: string) {
+    switch (option) {
+      case 'season':
+        this.historyTeamsFacade.getAllTimeTeamsStatBySeason(this.seasonOption);
+        break;
+      default:
+        return;
+    }
+  }
+}
