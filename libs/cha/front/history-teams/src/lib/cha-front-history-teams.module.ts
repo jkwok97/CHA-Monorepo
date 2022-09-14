eb7f63331ed919c718789ff421d10ec6c87c562d
Jeff Kwok <jkwok97@gmail.com>
2022-09-14 09:46:54 -0600

    connecting history teams state

--- libs/cha/front/history-teams/src/lib/cha-front-history-teams.module.ts
+++ libs/cha/front/history-teams/src/lib/cha-front-history-teams.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { HistoryTeamsEffects } from './+state/history-teams.effects';
+import { HistoryTeamsFacade } from './+state/history-teams.facade';
+import { reducer } from './+state/history-teams.reducer';
+import { HistoryTeamsService } from './services';
+
 import { ChaFrontHistoryTeamsRoutingModule } from './cha-front-history-teams-routing.module';
 
 import { HistoryTeamsComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontHistoryTeamsRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontHistoryTeamsRoutingModule,
+    StoreModule.forFeature('history-teams', reducer),
+    EffectsModule.forFeature([HistoryTeamsEffects]),
+  ],
   declarations: [HistoryTeamsComponent],
+  providers: [HistoryTeamsService, HistoryTeamsFacade, HistoryTeamsEffects],
 })
 export class ChaFrontHistoryTeamsModule {}
