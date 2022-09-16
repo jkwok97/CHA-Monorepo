190e486e90f05ad71eda373721f58c9b5409bf90
Jeff Kwok <jkwok97@gmail.com>
2022-09-16 11:07:28 -0600

    update history goalies module to connect state

--- libs/cha/front/history-goalies/src/lib/cha-front-history-goalies.module.ts
+++ libs/cha/front/history-goalies/src/lib/cha-front-history-goalies.module.ts
@@ -1,12 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+
+import { BladeButtonSelectComponent } from '@blade/angular/ui/button-select';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+
+import { HistoryGoaliesEffects } from './+state/history-goalies.effects';
+import { HistoryGoaliesFacade } from './+state/history-goalies.facade';
+import { reducer } from './+state/history-goalies.reducer';
+import { HistoryGoaliesService } from './services';
+
 import { ChaFrontHistoryGoaliesRoutingModule } from './cha-front-history-goalies-routing.module';
 
 import { HistoryGoaliesComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontHistoryGoaliesRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontHistoryGoaliesRoutingModule,
+    ProgressBarModule,
+    TableModule,
+    InputTextModule,
+    TooltipModule,
+    BladeAngularUiLayoutModule,
+    BladeButtonSelectComponent,
+    BladeDividerComponent,
+    BladeAngularUiCardModule,
+    StoreModule.forFeature('history-goalies', reducer),
+    EffectsModule.forFeature([HistoryGoaliesEffects]),
+  ],
   declarations: [HistoryGoaliesComponent],
+  providers: [
+    HistoryGoaliesService,
+    HistoryGoaliesFacade,
+    HistoryGoaliesEffects,
+  ],
 })
 export class ChaFrontHistoryGoaliesModule {}
