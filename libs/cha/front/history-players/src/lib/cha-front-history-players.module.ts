0681b5945bbacc4d0f06acfb2a27b654a8e71bb6
Jeff Kwok <jkwok97@gmail.com>
2022-09-16 08:51:05 -0600

    update history players module

--- libs/cha/front/history-players/src/lib/cha-front-history-players.module.ts
+++ libs/cha/front/history-players/src/lib/cha-front-history-players.module.ts
@@ -1,12 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+
+import { BladeButtonSelectComponent } from '@blade/angular/ui/button-select';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+
+import { HistoryPlayersEffects } from './+state/history-players.effects';
+import { HistoryPlayersFacade } from './+state/history-players.facade';
+import { reducer } from './+state/history-players.reducer';
+import { HistoryPlayersService } from './services';
+
 import { ChaFrontHistoryPlayersRoutingModule } from './cha-front-history-players-routing.module';
 
 import { HistoryPlayersComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontHistoryPlayersRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontHistoryPlayersRoutingModule,
+    ProgressBarModule,
+    TableModule,
+    InputTextModule,
+    TooltipModule,
+    BladeAngularUiLayoutModule,
+    BladeButtonSelectComponent,
+    BladeDividerComponent,
+    BladeAngularUiCardModule,
+    StoreModule.forFeature('history-players', reducer),
+    EffectsModule.forFeature([HistoryPlayersEffects]),
+  ],
   declarations: [HistoryPlayersComponent],
+  providers: [
+    HistoryPlayersService,
+    HistoryPlayersFacade,
+    HistoryPlayersEffects,
+  ],
 })
 export class ChaFrontHistoryPlayersModule {}
