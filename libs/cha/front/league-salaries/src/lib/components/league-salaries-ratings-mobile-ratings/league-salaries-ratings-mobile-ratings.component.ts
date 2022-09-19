93f00b7db8a8bd6af29ef767c925f672d69ef314
Jeff Kwok <jkwok97@gmail.com>
2022-09-19 09:33:13 -0600

    remove scss file

--- libs/cha/front/league-salaries/src/lib/components/league-salaries-ratings-mobile-ratings/league-salaries-ratings-mobile-ratings.component.ts
+++ libs/cha/front/league-salaries/src/lib/components/league-salaries-ratings-mobile-ratings/league-salaries-ratings-mobile-ratings.component.ts
@@ -4,7 +4,6 @@ import { PlayerRatingDto, GoalieRatingDto } from '@cha/shared/entities';
 @Component({
   selector: 'cha-front-league-salaries-ratings-mobile-ratings',
   templateUrl: './league-salaries-ratings-mobile-ratings.component.html',
-  styleUrls: ['./league-salaries-ratings-mobile-ratings.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeagueSalariesRatingsMobileRatingsComponent {
