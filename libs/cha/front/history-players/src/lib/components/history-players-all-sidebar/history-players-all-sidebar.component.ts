aadd145ffa4e83233eadae49419600b427f15adb
Jeff Kwok <jkwok97@gmail.com>
2022-09-21 15:38:54 -0600

    render initial table

--- libs/cha/front/history-players/src/lib/components/history-players-all-sidebar/history-players-all-sidebar.component.ts
+++ libs/cha/front/history-players/src/lib/components/history-players-all-sidebar/history-players-all-sidebar.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'cha-front-history-players-all-sidebar',
   templateUrl: './history-players-all-sidebar.component.html',
-  styleUrls: ['./history-players-all-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HistoryPlayersAllSidebarComponent {
--- libs/cha/front/home-players/src/lib/components/home-players-table-sidebar/home-players-table-sidebar.component.ts
+++ libs/cha/front/home-players/src/lib/components/home-players-table-sidebar/home-players-table-sidebar.component.ts
@@ -1,13 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cha-front-home-players-table-sidebar',
   templateUrl: './home-players-table-sidebar.component.html',
-  styleUrls: ['./home-players-table-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePlayersTableSidebarComponent implements OnInit {
-  constructor() {}
+export class HomePlayersTableSidebarComponent {
+  @Input() playerStats!: any;
+  @Input() statType!: string;
 
-  ngOnInit(): void {}
+  getPlayerPicture(id: string | undefined) {
+    if (id) {
+      return `https://cms.nhl.bamgrid.com/images/headshots/current/168x168/${id}@2x.jpg`;
+    } else {
+      return '';
+    }
+  }
 }
