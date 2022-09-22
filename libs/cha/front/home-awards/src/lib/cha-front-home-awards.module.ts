469c7e8b804eec78b602f0731ff78cb23466e9d8
Jeff Kwok <jkwok97@gmail.com>
2022-09-22 13:52:58 -0600

    hook up state mgmt to awards

--- libs/cha/front/home-awards/src/lib/cha-front-home-awards.module.ts
+++ libs/cha/front/home-awards/src/lib/cha-front-home-awards.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { HomeAwardsEffects } from './+state/home-awards.effects';
+import { HomeAwardsFacade } from './+state/home-awards.facade';
+import { reducer } from './+state/home-awards.reducer';
+import { HomeAwardsService } from './services';
+
 import { ChaFrontHomeAwardsRoutingModule } from './cha-front-home-awards-routing.module';
 
 import { HomeAwardsComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaFrontHomeAwardsRoutingModule],
+  imports: [
+    CommonModule,
+    ChaFrontHomeAwardsRoutingModule,
+    StoreModule.forFeature('home-awards', reducer),
+    EffectsModule.forFeature([HomeAwardsEffects]),
+  ],
   declarations: [HomeAwardsComponent],
+  providers: [HomeAwardsService, HomeAwardsFacade, HomeAwardsEffects],
 })
 export class ChaFrontHomeAwardsModule {}
