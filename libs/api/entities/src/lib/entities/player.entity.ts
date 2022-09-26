baf4d4b40d49879cb6de47d93e33c6cbe9bedad6
Jeff Kwok <jkwok97@gmail.com>
2022-09-26 12:52:26 -0600

    testing new column add

--- libs/api/entities/src/lib/entities/player.entity.ts
+++ libs/api/entities/src/lib/entities/player.entity.ts
@@ -44,6 +44,9 @@ export class Players_V2 {
   @Column()
   is_protected!: boolean;
 
+  @Column()
+  primary_position!: string;
+
   @OneToMany(() => Players_Stats_V2, (playerStats) => playerStats.player_id)
   playerStats!: Players_Stats_V2;
 
