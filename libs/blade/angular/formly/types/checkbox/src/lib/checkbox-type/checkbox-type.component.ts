3efa8aedf79ee5a78380d1649cddcbec06fd19c6
Jeff Kwok <jkwok97@gmail.com>
2022-09-29 15:37:32 -0600

    update checkbox type

--- libs/blade/angular/formly/types/checkbox/src/lib/checkbox-type/checkbox-type.component.ts
+++ libs/blade/angular/formly/types/checkbox/src/lib/checkbox-type/checkbox-type.component.ts
@@ -1,15 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FieldType, FieldTypeConfig, FormlyModule } from '@ngx-formly/core';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'blade-checkbox-type',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormlyModule, CheckboxModule],
   templateUrl: './checkbox-type.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxTypeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class CheckboxTypeComponent extends FieldType<FieldTypeConfig> {}
