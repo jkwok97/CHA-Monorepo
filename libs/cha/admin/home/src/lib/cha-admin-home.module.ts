34fea04510ce2a6ac1682b05f496b45f6c46ee68
Jeff Kwok <jkwok97@gmail.com>
2022-09-29 09:27:50 -0600

    home on admin side

--- libs/cha/admin/home/src/lib/cha-admin-home.module.ts
+++ libs/cha/admin/home/src/lib/cha-admin-home.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { LogoComponent } from '@cha/domain/logo';
+
 import { ChaAdminHomeRoutingModule } from './cha-admin-home-routing.module';
 
 import { HomeComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminHomeRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminHomeRoutingModule,
+    PerfectScrollbarModule,
+    BladeAngularUiLayoutModule,
+    LogoComponent,
+  ],
   declarations: [HomeComponent],
 })
 export class ChaAdminHomeModule {}
--- libs/cha/admin/home/src/lib/containers/home/home.component.ts
+++ libs/cha/admin/home/src/lib/containers/home/home.component.ts
@@ -1,8 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DisplayFacade } from '@cha/domain/core';
+import { ChaLogoEnum } from '@cha/shared/entities';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'cha-admin-home',
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  ChaLogo = ChaLogoEnum;
+  isMobile = false;
+  panelStyleMobile = {
+    width: '100%',
+    height: '77vh',
+    divHeight: '75vh',
+  };
+
+  panelStyleDesktop = {
+    width: '100%',
+    height: '83vh',
+    divHeight: '81vh',
+  };
+
+  constructor(private displayFacade: DisplayFacade) {
+    this.displayFacade.isMobile$
+      .pipe(first())
+      .subscribe((isMobile: boolean) => {
+        this.isMobile = isMobile;
+      });
+  }
+}
