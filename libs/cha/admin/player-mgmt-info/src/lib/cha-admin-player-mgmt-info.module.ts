ea6177d7fa2dfc2bd9c97881d82c9fca9b75e74c
Jeff Kwok <jkwok97@gmail.com>
2022-10-04 10:37:58 -0600

    render container

--- libs/cha/admin/player-mgmt-info/src/lib/cha-admin-player-mgmt-info.module.ts
+++ libs/cha/admin/player-mgmt-info/src/lib/cha-admin-player-mgmt-info.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+
 import { PlayerMgmtInfoFacade } from './+state/player-mgmt-info.facade';
 import { reducer } from './+state/player-mgmt-info.reducer';
 import { PlayerMgmtInfoEffects } from './+state/player-mgmt-info.effects';
@@ -15,6 +21,10 @@ import { PlayerMgmtInfoComponent } from './containers';
 @NgModule({
   imports: [
     CommonModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+    BladeAngularUiCardModule,
+    BladeDividerComponent,
     ChaAdminPlayerMgmtInfoRoutingModule,
     StoreModule.forFeature('player-mgmt-info', reducer),
     EffectsModule.forFeature([PlayerMgmtInfoEffects]),
--- libs/cha/admin/player-mgmt-info/src/lib/containers/player-mgmt-info/player-mgmt-info.component.ts
+++ libs/cha/admin/player-mgmt-info/src/lib/containers/player-mgmt-info/player-mgmt-info.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DisplayFacade } from '@cha/domain/core';
+import { PlayerDto } from '@cha/shared/entities';
+import { Observable, first } from 'rxjs';
+import { PlayerMgmtInfoFacade } from '../../+state/player-mgmt-info.facade';
 
 @Component({
   selector: 'cha-admin-player-mgmt-info',
@@ -6,4 +10,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./player-mgmt-info.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlayerMgmtInfoComponent {}
+export class PlayerMgmtInfoComponent {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  players$: Observable<PlayerDto[]>;
+
+  isMobile!: boolean;
+
+  constructor(
+    private playerMgmtInfoFacade: PlayerMgmtInfoFacade,
+    private displayFacade: DisplayFacade
+  ) {
+    this.isLoaded$ = this.playerMgmtInfoFacade.isLoaded$;
+    this.isLoading$ = this.playerMgmtInfoFacade.isLoading$;
+    this.players$ = this.playerMgmtInfoFacade.players$;
+
+    this.displayFacade.isMobile$
+      .pipe(first())
+      .subscribe((isMobile: boolean) => {
+        this.isMobile = isMobile;
+      });
+
+    this.playerMgmtInfoFacade.getPlayers();
+  }
+}
