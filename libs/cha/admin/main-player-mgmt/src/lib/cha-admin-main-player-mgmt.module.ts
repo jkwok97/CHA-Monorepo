2f8602f1d1a4d4b3cf08e1f768c4b8deaa8109cc
Jeff Kwok <jkwok97@gmail.com>
2022-10-04 09:25:06 -0600

    main player mgmt

--- libs/cha/admin/main-player-mgmt/src/lib/cha-admin-main-player-mgmt.module.ts
+++ libs/cha/admin/main-player-mgmt/src/lib/cha-admin-main-player-mgmt.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+
 import { ChaAdminMainPlayerMgmtRoutingModule } from './cha-admin-main-player-mgmt-routing.module';
 
 import { MainPlayerMgmtComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminMainPlayerMgmtRoutingModule],
+  imports: [
+    CommonModule,
+    PerfectScrollbarModule,
+    BladeAngularUiLayoutModule,
+    ChaAdminMainPlayerMgmtRoutingModule,
+  ],
   declarations: [MainPlayerMgmtComponent],
 })
 export class ChaAdminMainPlayerMgmtModule {}
--- libs/cha/admin/main-player-mgmt/src/lib/containers/main-player-mgmt/main-player-mgmt.component.ts
+++ libs/cha/admin/main-player-mgmt/src/lib/containers/main-player-mgmt/main-player-mgmt.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DisplayFacade } from '@cha/domain/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cha-admin-main-player-mgmt',
@@ -6,4 +8,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./main-player-mgmt.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainPlayerMgmtComponent {}
+export class MainPlayerMgmtComponent {
+  panelStyleMobile = {
+    width: '100%',
+    height: '77vh',
+  };
+
+  panelStyleDesktop = {
+    width: '100%',
+    height: '83vh',
+  };
+
+  isMobile$: Observable<boolean>;
+
+  constructor(private displayFacade: DisplayFacade) {
+    this.isMobile$ = this.displayFacade.isMobile$;
+  }
+}
