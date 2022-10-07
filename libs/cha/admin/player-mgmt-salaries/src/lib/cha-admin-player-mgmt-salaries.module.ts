66fc185ee743127e71b692e1538dbb09b21da6d3
Jeff Kwok <jkwok97@gmail.com>
2022-10-07 14:25:59 -0600

    connect state to module

--- libs/cha/admin/player-mgmt-salaries/src/lib/cha-admin-player-mgmt-salaries.module.ts
+++ libs/cha/admin/player-mgmt-salaries/src/lib/cha-admin-player-mgmt-salaries.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { PlayerMgmtSalariesEffects } from './+state/player-mgmt-salaries.effects';
+import { PlayerMgmtSalariesFacade } from './+state/player-mgmt-salaries.facade';
+import { reducer } from './+state/player-mgmt-salaries.reducer';
+
 import { ChaAdminPlayerMgmtSalariesRoutingModule } from './cha-admin-player-mgmt-salaries-routing.module';
 
 import { PlayerMgmtSalariesComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminPlayerMgmtSalariesRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminPlayerMgmtSalariesRoutingModule,
+    StoreModule.forFeature('player-mgmt-salaries', reducer),
+    EffectsModule.forFeature([PlayerMgmtSalariesEffects]),
+  ],
   declarations: [PlayerMgmtSalariesComponent],
+  providers: [PlayerMgmtSalariesEffects, PlayerMgmtSalariesFacade],
 })
 export class ChaAdminPlayerMgmtSalariesModule {}
