18fd940240bb0eaeca8272c41269b96b02d4ce5b
Jeff Kwok <jkwok97@gmail.com>
2022-10-17 08:43:20 -0600

    render container

--- libs/cha/admin/player-mgmt-player-current/src/lib/cha-admin-player-mgmt-player-current.module.ts
+++ libs/cha/admin/player-mgmt-player-current/src/lib/cha-admin-player-mgmt-player-current.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+
 import { PlayerMgmtPlayerCurrentEffects } from './+state/player-mgmt-player-current.effects';
 import { PlayerMgmtPlayerCurrentFacade } from './+state/player-mgmt-player-current.facade';
 import { reducer } from './+state/player-mgmt-player-current.reducer';
@@ -15,6 +21,10 @@ import { PlayerMgmtPlayerCurrentComponent } from './containers';
 @NgModule({
   imports: [
     CommonModule,
+    ProgressBarModule,
+    BladeAngularUiLayoutModule,
+    BladeAngularUiCardModule,
+    BladeDividerComponent,
     ChaAdminPlayerMgmtPlayerCurrentRoutingModule,
     StoreModule.forFeature('player-mgmt-player-current', reducer),
     EffectsModule.forFeature([PlayerMgmtPlayerCurrentEffects]),
--- libs/cha/admin/player-mgmt-player-current/src/lib/containers/player-mgmt-player-current/player-mgmt-player-current.component.ts
+++ libs/cha/admin/player-mgmt-player-current/src/lib/containers/player-mgmt-player-current/player-mgmt-player-current.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DisplayFacade } from '@cha/domain/core';
+import { StatPlayerAllDto } from '@cha/shared/entities';
+import { Observable, first } from 'rxjs';
+import { PlayerMgmtPlayerCurrentFacade } from '../../+state/player-mgmt-player-current.facade';
 
 @Component({
   selector: 'cha-admin-player-mgmt-player-current',
@@ -6,4 +10,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./player-mgmt-player-current.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlayerMgmtPlayerCurrentComponent {}
+export class PlayerMgmtPlayerCurrentComponent {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  players$: Observable<StatPlayerAllDto[]>;
+
+  isMobile!: boolean;
+
+  constructor(
+    private playerMgmtPlayerCurrentFacade: PlayerMgmtPlayerCurrentFacade,
+    private displayFacade: DisplayFacade
+  ) {
+    this.isLoaded$ = this.playerMgmtPlayerCurrentFacade.isLoaded$;
+    this.isLoading$ = this.playerMgmtPlayerCurrentFacade.isLoading$;
+    this.players$ = this.playerMgmtPlayerCurrentFacade.players$;
+
+    this.displayFacade.isMobile$
+      .pipe(first())
+      .subscribe((isMobile: boolean) => {
+        this.isMobile = isMobile;
+      });
+
+    this.playerMgmtPlayerCurrentFacade.getPlayers();
+  }
+}
