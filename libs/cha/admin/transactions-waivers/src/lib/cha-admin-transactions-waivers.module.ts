f02142288af73f12c8f312eb694b06b300b274d4
Jeff Kwok <jkwok97@gmail.com>
2022-10-18 08:03:11 -0600

    update imports

--- libs/cha/admin/transactions-waivers/src/lib/cha-admin-transactions-waivers.module.ts
+++ libs/cha/admin/transactions-waivers/src/lib/cha-admin-transactions-waivers.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { TransactionsWaiverssEffects } from './+state/transactions-waivers.effects';
+import { TransactionsWaiversFacade } from './+state/transactions-waivers.facade';
+import { reducer } from './+state/transactions-waivers.reducer';
+
 import { ChaAdminTransactionsWaiversRoutingModule } from './cha-admin-transactions-waivers-routing.module';
 
 import { TransactionsWaiversComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminTransactionsWaiversRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminTransactionsWaiversRoutingModule,
+    StoreModule.forFeature('transactions-waivers', reducer),
+    EffectsModule.forFeature([TransactionsWaiverssEffects]),
+  ],
   declarations: [TransactionsWaiversComponent],
+  providers: [TransactionsWaiverssEffects, TransactionsWaiversFacade],
 })
 export class ChaAdminTransactionsWaiversModule {}
