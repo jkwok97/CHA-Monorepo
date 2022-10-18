e495037d595f6c3f768ebb7a3b366130734fe04d
Jeff Kwok <jkwok97@gmail.com>
2022-10-18 08:13:51 -0600

    add dependency

--- libs/cha/admin/transactions-waivers/src/lib/cha-admin-transactions-waivers.module.ts
+++ libs/cha/admin/transactions-waivers/src/lib/cha-admin-transactions-waivers.module.ts
@@ -4,6 +4,13 @@ import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+import { OrderListModule } from 'primeng/orderlist';
+
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+
 import { TransactionsWaiverssEffects } from './+state/transactions-waivers.effects';
 import { TransactionsWaiversFacade } from './+state/transactions-waivers.facade';
 import { reducer } from './+state/transactions-waivers.reducer';
@@ -15,6 +22,11 @@ import { TransactionsWaiversComponent } from './containers';
 @NgModule({
   imports: [
     CommonModule,
+    ProgressBarModule,
+    OrderListModule,
+    BladeAngularUiCardModule,
+    BladeAngularUiLayoutModule,
+    BladeDividerComponent,
     ChaAdminTransactionsWaiversRoutingModule,
     StoreModule.forFeature('transactions-waivers', reducer),
     EffectsModule.forFeature([TransactionsWaiverssEffects]),
--- libs/cha/admin/transactions-waivers/src/lib/containers/transactions-waivers/transactions-waivers.component.ts
+++ libs/cha/admin/transactions-waivers/src/lib/containers/transactions-waivers/transactions-waivers.component.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DisplayFacade } from '@cha/domain/core';
+import { WaiversDto } from '@cha/shared/entities';
+import { Observable, first } from 'rxjs';
+import { TransactionsWaiversFacade } from '../../+state/transactions-waivers.facade';
 
 @Component({
   selector: 'cha-admin-transactions-waivers',
   templateUrl: './transactions-waivers.component.html',
-  styleUrls: ['./transactions-waivers.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionsWaiversComponent {}
+export class TransactionsWaiversComponent {
+  isLoading$: Observable<boolean>;
+  isLoaded$: Observable<boolean>;
+  waivers$: Observable<WaiversDto[]>;
+
+  isMobile!: boolean;
+
+  constructor(
+    private transactionsWaiversFacade: TransactionsWaiversFacade,
+    private displayFacade: DisplayFacade
+  ) {
+    this.isLoaded$ = this.transactionsWaiversFacade.isLoaded$;
+    this.isLoading$ = this.transactionsWaiversFacade.isLoading$;
+    this.waivers$ = this.transactionsWaiversFacade.waivers$;
+
+    this.displayFacade.isMobile$
+      .pipe(first())
+      .subscribe((isMobile: boolean) => {
+        this.isMobile = isMobile;
+      });
+
+    this.transactionsWaiversFacade.getWaivers();
+  }
+}
