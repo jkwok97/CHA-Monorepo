05694cf36958463ca08addf4700a20d4fbeae504
Jeff Kwok <jkwok97@gmail.com>
2022-10-26 16:01:09 -0600

    remove title after login on admin

--- apps/cha-admin/src/app/app.component.ts
+++ apps/cha-admin/src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthFacade } from '@cha/domain/auth';
 import { DisplayFacade, LeagueDataFacade } from '@cha/domain/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cha-hockey-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  loggedIn$: Observable<boolean>;
+  
   constructor(
     private displayFacade: DisplayFacade,
-    private leagueDataFacade: LeagueDataFacade
+    private leagueDataFacade: LeagueDataFacade,
+    private authFacade: AuthFacade,
   ) {
+    this.loggedIn$ = this.authFacade.loggingInComplete$;
     this.displayFacade.get();
     this.leagueDataFacade.getLeagueData();
     this.leagueDataFacade.getLeagueTeams();
