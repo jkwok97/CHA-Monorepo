dc9706007d2901077a68649cf3a09563ecc222ef
Jeff Kwok <jkwok97@gmail.com>
2022-10-28 08:50:20 -0600

    update imports

--- libs/cha/admin/draft-mgmt-entry/src/lib/cha-admin-draft-mgmt-entry.module.ts
+++ libs/cha/admin/draft-mgmt-entry/src/lib/cha-admin-draft-mgmt-entry.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { DraftMgmtEntryEffects } from './+state/draft-mgmt-entry.effects';
+import { DraftMgmtEntryFacade } from './+state/draft-mgmt-entry.facade';
+import { reducer } from './+state/draft-mgmt-entry.reducer';
+
 import { ChaAdminDraftMgmtEntryRoutingModule } from './cha-admin-draft-mgmt-entry-routing.module';
 
 import { DraftMgmtEntryComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminDraftMgmtEntryRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminDraftMgmtEntryRoutingModule,
+    StoreModule.forFeature('draft-mgmt-entry', reducer),
+    EffectsModule.forFeature([DraftMgmtEntryEffects]),
+  ],
   declarations: [DraftMgmtEntryComponent],
+  providers: [DraftMgmtEntryEffects, DraftMgmtEntryFacade],
 })
 export class ChaAdminDraftMgmtEntryModule {}
