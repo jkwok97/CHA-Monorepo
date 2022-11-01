a534b8e8d00abbb3ceddcce4ed78ba14118ba0b4
Jeff Kwok <jkwok97@gmail.com>
2022-11-01 11:22:44 -0600

    updating default to FA

--- dist/apps/cha-server/main.js
+++ dist/apps/cha-server/main.js
@@ -4873,7 +4873,7 @@ let ApiNhlService = class ApiNhlService {
                 teamlogo: true,
             },
             where: {
-                shortname: playerStatTeam === null || playerStatTeam === void 0 ? void 0 : playerStatTeam.team_name,
+                shortname: playerStatTeam ? playerStatTeam.team_name : 'FA',
             },
         });
     }
