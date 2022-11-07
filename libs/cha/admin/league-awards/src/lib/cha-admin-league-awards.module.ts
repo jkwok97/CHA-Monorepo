4391e44c920363ea3532454789dd184907eb2804
Jeff Kwok <jkwok97@gmail.com>
2022-11-07 14:22:02 -0700

    update module imports

--- libs/cha/admin/league-awards/src/lib/cha-admin-league-awards.module.ts
+++ libs/cha/admin/league-awards/src/lib/cha-admin-league-awards.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { LeagueAwardsEffects } from './+state/league-awards.effects';
+import { LeagueAwardsFacade } from './+state/league-awards.facade';
+import { reducer } from './+state/league-awards.reducer';
+
 import { ChaAdminLeagueAwardsRoutingModule } from './cha-admin-league-awards-routing.module';
 
 import { LeagueAwardsComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminLeagueAwardsRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminLeagueAwardsRoutingModule,
+    StoreModule.forFeature('league-awards', reducer),
+    EffectsModule.forFeature([LeagueAwardsEffects]),
+  ],
   declarations: [LeagueAwardsComponent],
+  providers: [LeagueAwardsEffects, LeagueAwardsFacade],
 })
 export class ChaAdminLeagueAwardsModule {}
