f03c62fd0d62ee23b92235ee24b9fbc5d58de319
Jeff Kwok <jkwok97@gmail.com>
2022-11-10 11:19:52 -0700

    render container

--- libs/cha/admin/league-conferences/src/lib/cha-admin-league-conferences.module.ts
+++ libs/cha/admin/league-conferences/src/lib/cha-admin-league-conferences.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+
 import { ChaAdminLeagueConferencesRoutingModule } from './cha-admin-league-conferences-routing.module';
+
 import { LeagueConferencesComponent } from './containers';
 
 @NgModule({
-  imports: [CommonModule, ChaAdminLeagueConferencesRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminLeagueConferencesRoutingModule,
+    BladeAngularUiCardModule,
+    BladeAngularUiLayoutModule,
+    BladeDividerComponent,
+  ],
   declarations: [LeagueConferencesComponent],
 })
 export class ChaAdminLeagueConferencesModule {}
--- libs/cha/admin/league-conferences/src/lib/containers/league-conferences/league-conferences.component.ts
+++ libs/cha/admin/league-conferences/src/lib/containers/league-conferences/league-conferences.component.ts
@@ -3,7 +3,29 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'cha-admin-league-conferences',
   templateUrl: './league-conferences.component.html',
-  styleUrls: ['./league-conferences.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeagueConferencesComponent {}
+export class LeagueConferencesComponent {
+  // isLoading$: Observable<boolean>;
+  // isLoaded$: Observable<boolean>;
+  // awards$: Observable<AwardDto[]>;
+
+  // isMobile!: boolean;
+
+  // constructor(
+  //   private leagueAwardsFacade: LeagueAwardsFacade,
+  //   private displayFacade: DisplayFacade
+  // ) {
+  //   this.isLoaded$ = this.leagueAwardsFacade.isLoaded$;
+  //   this.isLoading$ = this.leagueAwardsFacade.isLoading$;
+  //   this.awards$ = this.leagueAwardsFacade.awards$;
+
+  //   this.displayFacade.isMobile$
+  //     .pipe(first())
+  //     .subscribe((isMobile: boolean) => {
+  //       this.isMobile = isMobile;
+  //     });
+
+  //   this.leagueAwardsFacade.getAwards();
+  // }
+}
