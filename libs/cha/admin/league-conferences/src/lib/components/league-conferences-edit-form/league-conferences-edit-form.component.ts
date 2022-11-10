dc825aaa560e614a2f6270f01712e1b654de0845
Jeff Kwok <jkwok97@gmail.com>
2022-11-10 15:21:41 -0700

    render edit / add container and form

--- libs/cha/admin/league-conferences/src/lib/components/league-conferences-edit-form/league-conferences-edit-form.component.ts
+++ libs/cha/admin/league-conferences/src/lib/components/league-conferences-edit-form/league-conferences-edit-form.component.ts
@@ -1,13 +1,80 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { ConferenceDto } from '@cha/shared/entities';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'cha-admin-league-conferences-edit-form',
   templateUrl: './league-conferences-edit-form.component.html',
-  styleUrls: ['./league-conferences-edit-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeagueConferencesEditFormComponent implements OnInit {
-  constructor() {}
+  @Input() conference!: ConferenceDto | null;
 
-  ngOnInit(): void {}
+  form = new UntypedFormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields!: FormlyFieldConfig[];
+
+  ngOnInit(): void {
+    if (this.conference) {
+      this.patchForm();
+    }
+
+    this.createFields();
+  }
+
+  createFields() {
+    this.fields = [
+      {
+        fieldGroupClassName: 'w-full flex flex-wrap column-gap-2 row-gap-3',
+        fieldGroup: [this.isActiveField()],
+      },
+      {
+        fieldGroupClassName: 'w-full flex flex-wrap column-gap-2 row-gap-2',
+        fieldGroup: [this.nameField()],
+      },
+    ];
+  }
+
+  patchForm() {
+    this.model = {
+      isactive: this.conference?.isactive,
+      conferencename: this.conference?.conferencename,
+    };
+  }
+
+  isActiveField(): FormlyFieldConfig {
+    return {
+      key: 'isactive',
+      className: 'w-5',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Active Conference',
+      },
+    };
+  }
+
+  nameField(): FormlyFieldConfig {
+    return {
+      key: 'conferencename',
+      className: 'w-full md:w-3',
+      type: 'text-input',
+      templateOptions: {
+        label: 'Conference Name',
+        placeholder: 'Enter name',
+        required: true,
+      },
+      validation: {
+        messages: {
+          required: () => 'name is required',
+        },
+      },
+    };
+  }
 }
