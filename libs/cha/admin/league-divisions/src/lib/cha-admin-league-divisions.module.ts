5f78b5ae9fc8fffd8ac8f8422bf487640064758f
Jeff Kwok <jkwok97@gmail.com>
2022-11-10 11:18:41 -0700

    render container

--- libs/cha/admin/league-divisions/src/lib/cha-admin-league-divisions.module.ts
+++ libs/cha/admin/league-divisions/src/lib/cha-admin-league-divisions.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LeagueDivisionsComponent } from './containers';
+
+import { BladeAngularUiLayoutModule } from '@blade/angular/ui/layout';
+import { BladeAngularUiCardModule } from '@blade/angular/ui/card';
+import { BladeDividerComponent } from '@blade/angular/ui/divider';
+
 import { ChaAdminLeagueDivisionsRoutingModule } from './cha-admin-league-divisions-routing.module';
 
+import { LeagueDivisionsComponent } from './containers';
+
 @NgModule({
-  imports: [CommonModule, ChaAdminLeagueDivisionsRoutingModule],
+  imports: [
+    CommonModule,
+    ChaAdminLeagueDivisionsRoutingModule,
+    BladeAngularUiLayoutModule,
+    BladeAngularUiCardModule,
+    BladeDividerComponent,
+  ],
   declarations: [LeagueDivisionsComponent],
 })
 export class ChaAdminLeagueDivisionsModule {}
--- libs/cha/admin/league-divisions/src/lib/containers/league-divisions/league-divisions.component.ts
+++ libs/cha/admin/league-divisions/src/lib/containers/league-divisions/league-divisions.component.ts
@@ -1,13 +1,31 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'cha-admin-league-divisions',
   templateUrl: './league-divisions.component.html',
-  styleUrls: ['./league-divisions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeagueDivisionsComponent implements OnInit {
-  constructor() {}
+export class LeagueDivisionsComponent {
+  // isLoading$: Observable<boolean>;
+  // isLoaded$: Observable<boolean>;
+  // awards$: Observable<AwardDto[]>;
 
-  ngOnInit(): void {}
+  // isMobile!: boolean;
+
+  // constructor(
+  //   private leagueAwardsFacade: LeagueAwardsFacade,
+  //   private displayFacade: DisplayFacade
+  // ) {
+  //   this.isLoaded$ = this.leagueAwardsFacade.isLoaded$;
+  //   this.isLoading$ = this.leagueAwardsFacade.isLoading$;
+  //   this.awards$ = this.leagueAwardsFacade.awards$;
+
+  //   this.displayFacade.isMobile$
+  //     .pipe(first())
+  //     .subscribe((isMobile: boolean) => {
+  //       this.isMobile = isMobile;
+  //     });
+
+  //   this.leagueAwardsFacade.getAwards();
+  // }
 }
