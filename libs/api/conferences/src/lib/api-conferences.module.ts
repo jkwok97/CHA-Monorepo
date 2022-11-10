328bb65a1e33f68d2e0674c597a7e7148c469191
Jeff Kwok <jkwok97@gmail.com>
2022-11-10 13:50:56 -0700

    update module

--- libs/api/conferences/src/lib/api-conferences.module.ts
+++ libs/api/conferences/src/lib/api-conferences.module.ts
@@ -1,9 +1,13 @@
 import { Conferences_V2 } from '@api/entities';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConferencesController } from './controllers';
+import { ConferencesMiddleware } from './middlewares';
+import { ApiConferencesService } from './services';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Conferences_V2])],
-  controllers: [DivisionsController],
+  controllers: [ConferencesController],
   providers: [ApiConferencesService],
 })
 export class ApiConferencesModule {
